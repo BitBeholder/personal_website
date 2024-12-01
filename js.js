c02b5c52c07b9fad75b77f08fecfc1edadb976f5
bitbeholder <e.sario24@gmail.com>
2024-11-30 17:40:49 -0800

    projects edit

--- js.js
+++ js.js
@@ -1,10 +1,33 @@
-document.getElementById("toggle-images").addEventListener("click", function () {
-    const gallery = document.getElementById("image-gallery");
-    if (gallery.classList.contains("hidden")) {
-        gallery.classList.remove("hidden");
+document.getElementById("toggle-images-1").addEventListener("click", function () {
+    const gallery1 = document.getElementById("image-gallery-1");
+    if (gallery1.classList.contains("hidden")) {
+        gallery1.classList.remove("hidden");
         this.textContent = "Hide Pictures";
     } else {
-        gallery.classList.add("hidden");
+        gallery1.classList.add("hidden");
         this.textContent = "Show Pictures";
     }
 });
+
+document.getElementById("toggle-images-2").addEventListener("click", function () {
+    const gallery2 = document.getElementById("image-gallery-2");
+    if (gallery2.classList.contains("hidden")) {
+        gallery2.classList.remove("hidden");
+        this.textContent = "Hide Pictures";
+    } else {
+        gallery2.classList.add("hidden");
+        this.textContent = "Show Pictures";
+    }
+});
+
+document.getElementById("toggle-images-3").addEventListener("click", function () {
+    const gallery2 = document.getElementById("image-gallery-3");
+    if (gallery2.classList.contains("hidden")) {
+        gallery2.classList.remove("hidden");
+        this.textContent = "Hide Pictures";
+    } else {
+        gallery2.classList.add("hidden");
+        this.textContent = "Show Pictures";
+    }
+});
+
